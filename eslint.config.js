6009b42e81d51c7e707b76069cd94645d9bff58b
陈晓刚 <Just-focus@users.noreply.github.com>
2025-07-25 18:43:19 +0800

    feat: :sparkles: 修改 eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,45 @@
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import globals from 'globals'
 import importSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.ts'],
-  ignores: ['*.js', '**/dist/*'],
-  rules: {
-    'unicorn/no-array-reduce': 'off',
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^\\w'], // 表示 node 内置模块
-          ['^@\\w'], // 表示以 @ 开头的路径
-          ['^@/'], // 表示以 @ 开头的自定义识别路径
-          ['^\\u0000'],
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$']
-        ]
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**']
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    name: 'main',
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.node
+      },
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname // __dirname for commonJs
       }
-    ],
-    'simple-import-sort/exports': 'error',
-    '@typescript-eslint/no-explicit-any': 'off'
-  },
-  languageOptions: {
-    parser: tseslint.parser,
-    globals: {
-      ...globals.node
     },
-    parserOptions: {
-      tsconfigRootDir: import.meta.dirname // __dirname for commonJs
+    plugins: {
+      'simple-import-sort': importSort
+    },
+    rules: {
+      'unicorn/no-array-reduce': 'off',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\w'], // 表示 node 内置模块
+            ['^@\\w'], // 表示以 @ 开头的路径
+            ['^@/'], // 表示以 @ 开头的自定义识别路径
+            ['^\\u0000'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$']
+          ]
+        }
+      ],
+      'simple-import-sort/exports': 'error',
+      '@typescript-eslint/no-explicit-any': 'off'
     }
-  },
-  plugins: {
-    'simple-import-sort': importSort
   }
-})
+)
